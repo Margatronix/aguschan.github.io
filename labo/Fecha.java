ab83be6d4138594103c1e1fe4fc391d9411336b0
Marga <82117999+aguschan@users.noreply.github.com>
2022-04-01 21:19:57 -0300

    Add files via upload

---
+++ labo/Fecha.java
@@ -0,0 +1,35 @@
+public class Fecha {
+    private int dia;
+    private int mes;
+    private int año;
+
+    public Fecha {
+        this.dia = 1;
+        this.mes = 1;
+        this.año = 1;
+    }
+
+    public int getDia {
+        return dia;
+    }
+
+    public int getMes {
+        return mes;
+    }
+
+    public int getAño {
+        return año;
+    }
+
+    public int setDia(int dia){
+        this.dia = dia;
+    }
+
+    public int setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public int setAño(int año) {
+        this.año = año;
+    }
+}
