1dfc69c91b6aa028e5f244b74dbbdcfa3f7f8752
Marga <82117999+aguschan@users.noreply.github.com>
2022-04-27 08:50:49 -0300

    ejercicio 1 dia 26

--- labo/Clientes.java
+++ labo/Clientes.java
@@ -1,10 +1,21 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class Clientes {
     private int id;
     private int edad;
     private String nombre;
-    private ArrayList<Libros> librosComprados = new ArrayList<Libros>();
+    private ArrayList<Libros> librosComprados;
+     private HashMap<Libros, Integer> cantidadLibros;
+
+    public Clientes(int id, int edad, String nombre, ArrayList<Libros> librosComprados, HashMap<Libros, Integer> cantidadLibros) {
+        this.id = id;
+        this.edad = edad;
+        this.nombre = nombre;
+        this.librosComprados = new ArrayList<Libros>();
+        this.cantidadLibros = new HashMap<Libros, Integer>();
+    }
 
     public int getId() {
         return id;
@@ -18,9 +29,29 @@ public class Clientes {
         return nombre;
     }
 
+    public HashMap<Libros, Integer> getCantidadLibros() {
+        return cantidadLibros;
+    }
+
     public ArrayList<Libros> getLibrosComprados() {
         return librosComprados;
     }
 
+    public void llenarCantidadLibros() {
+        Libros libroejemplo = new Libros();
+        int cantidad = 0;
+        HashSet<Integer> ids = new HashSet<Integer>();
+        for (Libros i:this.librosComprados) {
+            ids.add(i.getId());
+        }
+            for (int e:ids) {
+                for (Libros a:this.librosComprados) {
+                    if (e == a.getId()) {
+                        libroejemplo = a;
+                        cantidad ++;
+                    }
+                }
+                cantidadLibros.put(libroejemplo, cantidad);
+            }
+    }
 }
-
--- labo/SistemaLibreria.java
+++ labo/SistemaLibreria.java
@@ -1,13 +1,27 @@
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.HashMap;
 
 public class SistemaLibreria {
     private String nombre;
     private ArrayList<Libros> librosEnVenta = new ArrayList<Libros>();
     private ArrayList<Clientes> clientes = new ArrayList<Clientes>();
+    private HashSet<Clientes> clientes2 = new HashSet<Clientes>();
     private ArrayList<Integer> librosEnOferta = new ArrayList<Integer>();
     private ArrayList<Libros> librosVendidos = new ArrayList<Libros>();
+    private HashSet<String> editorialesDescuento = new HashSet<String>();
+    private HashMap<Integer, Float> clienteYMontoTotal = new HashMap<Integer, Float>();
 
-    Clientes c = new Clientes();
+    public SistemaLibreria(String nombre, ArrayList<Libros> librosEnVenta, ArrayList<Clientes> clientes, HashSet<Clientes> clientes2, ArrayList<Integer> librosEnOferta, ArrayList<Libros> librosVendidos, HashSet<String> editorialesDescuento, HashMap<Integer, Float> clienteYMontoTotal) {
+        this.nombre = nombre;
+        this.librosEnVenta = new ArrayList<Libros>();
+        this.clientes = new ArrayList<Clientes>();
+        this.clientes2 = new HashSet<Clientes>();
+        this.librosEnOferta = new ArrayList<Integer>();
+        this.librosVendidos = new ArrayList<Libros>();
+        this.editorialesDescuento = new HashSet<String>();
+        this.clienteYMontoTotal = new HashMap<Integer, Float>();
+    }
 
     public ArrayList<Clientes> clientesFrecuentes() {
         ArrayList<Clientes> clientesfrecuentes = new ArrayList<Clientes>();
@@ -22,10 +36,14 @@ public class SistemaLibreria {
         ArrayList<Libros> librosVendidos = new ArrayList<Libros>();
         int contador = 0;
         for (Libros i: librosEnVenta) {
-            for (int j = 0; j < clientes.size(); j++) {
-                if (i == c.getLibrosComprados().get(j)) {
-                    contador ++;
-                }
+            for (Clientes messi: clientes2) {
+                for (int j = 0; j < clientes.size(); j++) {
+                    if (i == messi.getLibrosComprados().get(j)) {
+                        contador ++;
+                    }
+            }
+
+
             }
             if (contador > 100) {
                 librosVendidos.add(i);
@@ -34,13 +52,13 @@ public class SistemaLibreria {
         }
         return librosVendidos;
     }
-     public ArrayList<Integer> edades(){
+    public ArrayList<Integer> edades(){
         ArrayList<Integer> edadesClientesFrec = new ArrayList<Integer>();
-         for (Clientes i: clientesFrecuentes()) {
-             edadesClientesFrec.add(i.getEdad());
-         }
+        for (Clientes i: clientesFrecuentes()) {
+            edadesClientesFrec.add(i.getEdad());
+        }
         return edadesClientesFrec;
-     }
+    }
     public ArrayList<Libros> librosCaros() {
         ArrayList<Libros> librosMasCaros = new ArrayList<Libros>();
         for (Libros i: librosEnVenta) {
@@ -92,5 +110,33 @@ public class SistemaLibreria {
                 i.setPrecio(i.getPrecio()-35*i.getPrecio()/100);
             }
         }
+    }
+    public float precioLibrosComprados(int id_cliente) {
+        float montoTotal = 0;
+        for (Clientes i:clientes) {
+            if (id_cliente == i.getId()) {
+                montoTotal = precioLibros(i.getLibrosComprados());
+            }
+        }
+        return montoTotal;
+    }
+    public void agregarEditorial(String editorialDesc) {
+        editorialesDescuento.add(editorialDesc);
+    }
+    public void pagoCliente() {
+        for (Clientes i:clientes) {
+            clienteYMontoTotal.put(i.getId(), precioLibros(i.getLibrosComprados()));
+        }
+    }
+    public void mostrarCompra (int id) {
+        System.out.println("libros y sus respectivas unidades:");
+        for(Clientes messi : clientes2) {
+            if (id == messi.getId()) {
+                for (Libros i : messi.getCantidadLibros().keySet()) {
+                    System.out.println(i.getNombre() + " " + messi.getCantidadLibros().get(i));
+                }
+            }
+        }
+        System.out.println("el monto total es: "+precioLibrosComprados(id));
     }
 }
